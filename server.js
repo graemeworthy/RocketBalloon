93ea217cdb9e27905dc111ebef4772c691a487a7
Graeme Worthy <graeme@workben.ch>
2014-05-13 23:21:14 -0700

    trying to heroku, some port stuff

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ send404 = function(res) {
 };
 
 // server.listen(8347);
-server.listen();
+server.listen(process.env.PORT || 8347);
 var socket = io.listen(server),
     buffer = [];
 var clients = [];
